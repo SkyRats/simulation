eda67878ed00d4f1cab1ba3879469d772d7c3320
Henrique-BO <henrique.barrosoliveira@usp.br>
2020-07-12 13:43:47 -0300

    Added README.md and convenience setup script

---
+++ README.md
@@ -0,0 +1,21 @@
+# Skyrats simulation package
+
+ROS package for PX4-Gazebo-ROS Software-In-The-Loop simulations for the Skyrats team.
+
+## Contents
+
+- `launch`: launchfiles for starting gazebo worlds with ROS
+- `models`: SDF and URDF models
+- `plugins`: Gazebo plugins
+- `scripts`: Setup scripts
+- `worlds`: Gazebo worlds
+
+## Launching the simulation
+
+```bash
+# Convenience script for building the PX4 Firmware and setting up Environment Variables
+source scripts/setup.bash
+
+# Plain SITL simulation with Iris drone
+roslaunch px4 mavros_posix_sitl.launch
+```
---
+++ scripts/setup.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [ "$#" == 0 ]; then
+    echo "Using default values"
+    SIMULATION_DIR="$( cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd )"
+    FIRMWARE_DIR="$HOME/src/Firmware"
+    FIRMWARE_BUILD_DIR="$HOME/src/Firmware/build/px4_sitl_default"
+elif [ "$#" == 3 ]; then
+    SIMULATION_DIR=$1       # Path of the simulation package
+    FIRMWARE_DIR=$2         # Path of the PX4 Firmware
+    FIRMWARE_BUILD_DIR=$3   # Path of the SITL build directory
+else
+    echo -e "usage: source setup.bash simulation_dir firmware_dir firmware_build_dir"
+    return 1
+fi
+
+# Build PX4 SITL
+cd ${FIRMWARE_DIR}
+DONT_RUN=1 make px4_sitl_default gazebo
+
+# Setup Gazebo to find this package's models and plugins
+export GAZEBO_MODEL_PATH=${SIMULATION_DIR}/models:${GAZEBO_MODEL_PATH}
+export GAZEBO_PLUGIN_PATH=${SIMULATION_DIR}/../../devel/lib:${GAZEBO_PLUGIN_PATH}
+
+# Setup Gazebo to find PX4
+source ${FIRMWARE_DIR}/Tools/setup_gazebo.bash ${FIRMWARE_DIR} ${FIRMWARE_BUILD_DIR}
+
+# Setup ROS to find the PX4 packages
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:${FIRMWARE_DIR}
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:${FIRMWARE_DIR}/Tools/sitl_gazebo
+
+echo -e "ROS_PACKAGE_PATH $ROS_PACKAGE_PATH"
