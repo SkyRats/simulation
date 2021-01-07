8916a4ede0aa9ef26dbf5a7f4587544a9be07d72
Felipe Cardenas Lima Namour <felipe.cln@usp.br>
2021-01-07 12:54:00 -0300

    Updates to swarm simulation

--- plugins/attach_rope.cc
+++ plugins/attach_rope.cc
@@ -10,27 +10,30 @@ namespace gazebo {
         physics::JointPtr joint_2;
 
         ignition::math::Vector3d  z_axis = ignition::math::Vector3d(0,0,1);
+        ignition::math::Vector3d  null_axis = ignition::math::Vector3d(0,0,0);
+        ignition::math::Vector4d  null_axis4D = ignition::math::Vector4d(1,0,0,0);
+
+        //ignition::math::Pose3<ignition::math::Vector3d> nullPose = ignition::math::Pose3<ignition::math::Vector3d>(null_axis, null_axis4D);
         
         physics::ModelPtr corda_0 = this->world->ModelByName("swarm_box::swarm_box::Corda_0"); //esquerda
         physics::ModelPtr iris_0 = this->world->ModelByName("iris_0");  //esquerda
         physics::ModelPtr iris_1 =  this->world->ModelByName("iris_1"); //direita
         physics::ModelPtr corda_1 = this->world->ModelByName("swarm_box::swarm_box::Corda"); //direita
+        physics::ModelPtr POSREF = this->world->ModelByName("POS_REF");
+        physics::ModelPtr NEGREF = this->world->ModelByName("NEG_REF");
+        physics::ModelPtr NULLREF = this->world->ModelByName("NULL_REF");
         // Joint_1
         joint_1 = this->world->Physics()->CreateJoint("ball", iris_0);
-        joint_1->Load(iris_0->GetLink("base_link"), corda_0->GetLink("link_10"), corda_0->GetLink("link_10")->WorldPose());
+        joint_1->Load(iris_0->GetLink("base_link"), corda_0->GetLink("link_10"), NEGREF->WorldPose());
         joint_1->Attach(iris_0->GetLink("base_link"), corda_0->GetLink("link_10"));
         //joint_1->SetAxis(0, z_axis);
-        //joint_1->SetHighStop(0, high_stop_ex);
-        //joint_1->SetParam("cfm", 0, cfm_ex);
         joint_1->Init();
 
-        //Joint_2
+        //Joint_2 corda_0->GetLink("link_10")->WorldPose()
         joint_2 = this->world->Physics()->CreateJoint("ball", iris_1);
-        joint_2->Load(iris_1->GetLink("base_link"), corda_1->GetLink("link_10"), corda_0->GetLink("link_10")->WorldPose());//math::Pose()
+        joint_2->Load(iris_1->GetLink("base_link"), corda_1->GetLink("link_10"), POSREF->WorldPose());//math::Pose()
         joint_2->Attach(iris_1->GetLink("base_link"), corda_1->GetLink("link_10"));
         //joint_2->SetAxis(0, z_axis);
-        //joint_2->SetHighStop(0, high_stop_ex);
-        //joint_2->SetParam("cfm", 0, cfm_ex);
         joint_2->Init();
         //link_10 e o mais em cima da corda
         //base_link do iris
