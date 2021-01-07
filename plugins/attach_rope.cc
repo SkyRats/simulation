c6cd9c1152fa1928f2cc4353e28a034292ece003
Felipe Cardenas Lima Namour <felipe.cln@usp.br>
2021-01-07 13:06:45 -0300

    Finished swarm outdoor simulation

--- plugins/attach_rope.cc
+++ plugins/attach_rope.cc
@@ -23,16 +23,16 @@ namespace gazebo {
         physics::ModelPtr NEGREF = this->world->ModelByName("NEG_REF");
         physics::ModelPtr NULLREF = this->world->ModelByName("NULL_REF");
         // Joint_1
-        joint_1 = this->world->Physics()->CreateJoint("ball", iris_0);
-        joint_1->Load(iris_0->GetLink("base_link"), corda_0->GetLink("link_10"), NEGREF->WorldPose());
-        joint_1->Attach(iris_0->GetLink("base_link"), corda_0->GetLink("link_10"));
+        joint_1 = this->world->Physics()->CreateJoint("ball", corda_0);
+        joint_1->Load(corda_0->GetLink("link_10"), iris_0->GetLink("base_link"), NULLREF->WorldPose());
+        joint_1->Attach(corda_0->GetLink("link_10"), iris_0->GetLink("base_link"));
         //joint_1->SetAxis(0, z_axis);
         joint_1->Init();
 
         //Joint_2 corda_0->GetLink("link_10")->WorldPose()
-        joint_2 = this->world->Physics()->CreateJoint("ball", iris_1);
-        joint_2->Load(iris_1->GetLink("base_link"), corda_1->GetLink("link_10"), POSREF->WorldPose());//math::Pose()
-        joint_2->Attach(iris_1->GetLink("base_link"), corda_1->GetLink("link_10"));
+        joint_2 = this->world->Physics()->CreateJoint("ball", corda_1);
+        joint_2->Load(corda_1->GetLink("link_10"), iris_1->GetLink("base_link"), NULLREF->WorldPose());//math::Pose()
+        joint_2->Attach(corda_1->GetLink("link_10") ,iris_1->GetLink("base_link"));
         //joint_2->SetAxis(0, z_axis);
         joint_2->Init();
         //link_10 e o mais em cima da corda
