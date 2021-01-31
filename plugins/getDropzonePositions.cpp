4ae578a88b31925317c4f38e36a927fdbecc4740
Felipe Cardenas Lima Namour <felipe.cln@usp.br>
2021-01-31 00:20:27 -0300

    Fixed bugs in dropzones plugin

--- plugins/getDropzonePositions.cpp
+++ plugins/getDropzonePositions.cpp
@@ -1,3 +1,4 @@
+ 
 #include "../include/simulation/getDropzonePositions.h"
 
 static const double equatorial_radius = 6378197.0;
@@ -28,9 +29,13 @@ namespace gazebo
         reference_heading_ = DEFAULT_REFERENCE_HEADING;
         reference_altitude_ = DEFAULT_REFERENCE_ALTITUDE;
 
-        _sdf->GetElement("latitude_deg")->GetValue()->Get(reference_latitude_);
-        _sdf->GetElement("longitude_deg")->GetValue()->Get(reference_longitude_);
-        _sdf->GetElement("heading_deg")->GetValue()->Get(reference_heading_);
+        //_sdf->GetElement("latitude_deg")->GetValue()->Get(reference_latitude_);
+        //_sdf->GetElement("longitude_deg")->GetValue()->Get(reference_longitude_);
+        //_sdf->GetElement("heading_deg")->GetValue()->Get(reference_heading_);
+        reference_latitude_ = 18.635216;
+        reference_longitude_ = -98.522657;
+        reference_heading_ = 0;
+
         reference_heading_ *= M_PI/180.0;
 
         double temp = 1.0 / (1.0 - excentrity2 * sin(reference_latitude_ * M_PI/180.0) * sin(reference_latitude_ * M_PI/180.0));
@@ -46,6 +51,14 @@ namespace gazebo
         ignition::math::Pose3d dz_clean_pose = _parent->WorldPose();
         ignition::math::Vector3<double> dzc_p = dz_clean_pose.Pos();
         double CP_V[3] = {dzc_p.X(), dzc_p.Y(), dzc_p.Z() };
-        gzmsg << "Dropzone pose:\n\t Latitude = " << reference_latitude_  + ( cos(reference_heading_) * CP_V[0] + sin(reference_heading_) * CP_V[1]) / radius_north_ * 180.0/M_PI << "\n\t Longitude = " << reference_longitude_ - (-sin(reference_heading_) * CP_V[0] + cos(reference_heading_) * CP_V[1]) / radius_east_  * 180.0/M_PI << "\n"<< std::endl;
+
+        physics::WorldPtr puebla = _parent->GetWorld();
+        physics::ModelPtr iris = puebla->ModelByName("iris");
+        ignition::math::Pose3d iris_pose = iris->WorldPose();
+        ignition::math::Vector3<double> iris_p = iris_pose.Pos();
+        double Iris_V[3] = {iris_p.X(), iris_p.Y(), iris_p.Z() };
+
+        gzmsg << "Dropzone GPS:\n\t Latitude = " << reference_latitude_  + ( cos(reference_heading_) * CP_V[0] + sin(reference_heading_) * CP_V[1]) / radius_north_ * 180.0/M_PI << "\n\t Longitude = " << reference_longitude_ - (-sin(reference_heading_) * CP_V[0] + cos(reference_heading_) * CP_V[1]) / radius_east_  * 180.0/M_PI << "\n"<< std::endl;
+        gzmsg << "Dropzone pose:"<< "\n\tX = " << CP_V[0] - Iris_V[0]  << "\n\tY = " << CP_V[1] - Iris_V[1] << "\n\tZ = " << CP_V[2] - Iris_V[2] << std::endl;
     }
     };
